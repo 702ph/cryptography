e16df5762c2b1102fad7bca020f60b6131cfaa3e
Shuya Fuchigami <>
2019-10-17 22:11:20 +0200

    small fix

--- src/lab01/Exercise01.java
+++ src/lab01/Exercise01.java
@@ -1,6 +1,5 @@
 package lab01;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -40,13 +39,15 @@ public class Exercise01 {
         secretMessages.add(new SecretMessage("01000110100110110001110001100010101111001110010100101111011010110100001000110011", "00010101110011100101111000110001111010001010010001100001001111110000101101100101"));
         secretMessages.add(new SecretMessage("00110011000001110010010000001111000001001101010100000001110111001010110011110011", "01110100011100100101000001101010011010101001010001100011101110010100001010010111"));
         secretMessages.add(new SecretMessage("01110110001001110001111101010011100110100100110111001101001010001000001110100111","00001111010100110111011000100000111010000010100010111011010000011110110111110010"));
+        secretMessages.add(new SecretMessage("00110010111111011111010111000111100001001011001011001111001010000101001101100010","01100010101011111011000010001001110111101111111010001110011111010001011000110000"));
 
 
         // decryption
         for(SecretMessage message : secretMessages) {
             printAsBinary("encrypted :", message.encryptedText);
             printAsBinary("key       :", message.key);
-            printAsString("decrypted :", xor(message.encryptedText, message.key));
+            printAsBinary("decrypted :", xor(message.encryptedText, message.key));
+            printAsString("PlainText :", xor(message.encryptedText, message.key));
         }
     }
 
